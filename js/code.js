0e80cc8e7b233e149bc1f359af138f55f06708c7
AGUSTÍN SIROTINSKY <46645963@ort>
2023-04-03 09:05:20 -0300

    3/4 2

--- js/code.js
+++ js/code.js
@@ -1,6 +1,7 @@
 const elemento = () => (document.getElementById("elemento").value)
 let ElementosArray=[]
 let valor=0;
+//Agregar los valores al array
 function AgregarAArray(elemento){
     let hora=Date.now()
     const elementoCompleto
@@ -22,24 +23,26 @@ function AgregarAArray(elemento){
     lista.appendChild(item)
     console.log(ElementosArray)
 }
+//Calcula tiempo desde que se creo hasta que se cerro
 function CalcularTiempo(tiempoDeId){
-
+    let horaActual=Date.now()
 }
+//Activado por el boton
 function Agregar(){
     let elem=elemento()
     let resultado
     resultado=elem
     AgregarAArray(resultado)
 }
+//Tachar elementos clickeados
 var list = document.querySelector('ul');
 list.addEventListener('click', function(evt) {
   console.log("lista clickeada");
   if(evt.target.tagName === "LI") {
     evt.target.classList.toggle("checked");
-    console.log(evt.target.id)
     for(let i=0;i<=ElementosArray.length;i++){
         if(evt.target.id==ElementosArray[i].id){
-
+            CalcularTiempo(ElementosArray[i].id)
         }
     }
   }
